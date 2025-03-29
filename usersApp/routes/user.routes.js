54c20648cc7fe1b0977b320b3b61abf82a35de34
lampros alexandris <lamprospatmos19@gmail.com>
2025-03-29 02:37:11 +0200

    post

---
+++ usersApp/routes/user.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router(); 
+
+const userController = require('../controllers/user.controller');
+
+router.get('/', userController.findAll);
+router.get('/:username', userController.findOne)
+router.post('/', userController.findAll);
+
+module.exports = router;
+
+
+
+
