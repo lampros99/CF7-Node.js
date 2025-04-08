24f498930101fec6749f1c835c27a7ae9cfcea50
lampros alexandris <lamprospatmos19@gmail.com>
2025-04-09 01:53:19 +0300

    updated

--- usersApp/controllers/user.controller.js
+++ usersApp/controllers/user.controller.js
@@ -2,6 +2,8 @@ const User = require('../models/user.model');
 const userService = require('../services/user.services');
 const bcrypt = require('bcrypt');
 
+const logger = require('../logger/logger')
+
 
 exports.findAll = async(req, res) => {
   console.log("Find all users from collection users");
@@ -10,8 +12,12 @@ exports.findAll = async(req, res) => {
     // const result = await User.find();
     const result = await userService.findAll();
     res.status(200).json({status: true, data: result});
+    logger.info("INFO, Success in reading all users");
+    logger.warn("INFO, Success in reading all users")
+    logger.error("Message With Error")
   } catch (err) {
     console.log("Problem in reading users", err);
+    logger.error("Error")
     res.status(400).json({status:false, data: err});
   }
 }
