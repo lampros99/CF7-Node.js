089f77e2ce04e3feeabc15772bf720d4bd400d49
lampros alexandris <lamprospatmos19@gmail.com>
2025-04-20 13:12:39 +0300

    routes updated

--- usersApp/routes/auth.routes.js
+++ usersApp/routes/auth.routes.js
@@ -5,5 +5,9 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post('/login', authController.login);
+router.get('/google/callback', authController.googleLogin);
 
 module.exports = router;
+//https://accounts.google.com/o/oauth2/auth?
+// client_id=928044971801-5oapusuv5c7fo33fp53vqbgio33ng7rv.apps.googleusercontent.com&
+// redirect_uri=http://localhost:3000/api/auth/google/callback&response_type=code&scope=email%20profile&access_type=offline
