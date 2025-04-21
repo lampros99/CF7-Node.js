2576242545b9ba856e3d1f767d215f512b067637
lampros alexandris <lamprospatmos19@gmail.com>
2025-04-21 17:32:57 +0300

    jestApp

---
+++ jestApp/index.js
@@ -0,0 +1,19 @@
+const mathOperations = {
+    sum: function (a, b) {
+        return a + b;
+    },
+
+    dif: function(a, b) {
+        return a - b;
+    },
+    product: function(a, b) {
+        return a*b;
+    },
+
+    divide: function(a, b){
+        return a / b;
+    },
+}
+
+
+module.exports = mathOperations;
