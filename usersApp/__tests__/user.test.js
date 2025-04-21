4483f34bf0ac618f08f184065bd1839594a8c7c8
lampros alexandris <lamprospatmos19@gmail.com>
2025-04-21 17:32:09 +0300

    updates

---
+++ usersApp/__tests__/user.test.js
@@ -0,0 +1,68 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+const authService = require('../services/auth.service');
+const userService = require('')
+const app = require('../app');
+require('dotenv').config();
+
+// Connecting to MongoDB before each test
+
+beforeEach(async () => {
+    await mongoose.connect(process.env.MONGODB_URI)
+    .then(
+        () => {console.log("Connection to MongoDB establiched for Jest")},
+        err => { console.log("Failed to connect to MongoDB for Jest", err) }
+    );
+});
+
+// Close connection to MongoDB
+afterEach(async () => {
+    await mongoose.connection.close();
+})
+
+describe("GET Requests for /api/users", () => {
+    let token;
+
+    beforeAll(()=> {
+    user = {
+    username: "admin",
+    email: "admin@aueb.gr",
+    roles:["EDITOR","READER","ADMIN"]
+    };
+    token = authService.generateAccessToken(user);
+    });
+
+    it('GET Returns all users', async () => {
+            const res = await request(app)
+            .get('/api/users')
+            .set('Authorization', `Bearer ${token}`);
+
+        expect(res.statusCode).toBe(200)
+        expect(res.body.status).toBeTruthy();
+        expect(res.body.data.length).toBeGreaterThan(0);
+    }, 50000)
+})
+
+
+describe("Request for /api/users/:username", () => {
+    beforeAll(() =>{
+        user = {
+            username: "admin",
+            email: "admin@aueb.gr",
+            roles:["EDITOR","READER","ADMIN"]
+            };
+        token = authService.generateAccessToken(user);
+    });
+    it("Get returns specific user", async() => {
+
+
+        const res = await request(app)
+        .get('/api/users')
+        .set('Authorization', `Bearer ${token}`);
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body.status).toBeTruthy();
+    expect(res.data.username).toBe('user1');
+    expect(res.data.email).toBe('user1@aueb.gr');
+    })
+})
--- usersApp/server.js
+++ usersApp/server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const app = require('./app');
 const port = 3000;
 
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect(process.env.MONGODB_URI)
 .then(
     () => {
       console.log("Connection to MongoDB established");
