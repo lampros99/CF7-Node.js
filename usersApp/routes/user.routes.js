c7ad3590e651ffb6d16b770c015c8d77df0c371e
lampros alexandris <lamprospatmos19@gmail.com>
2025-04-25 20:53:07 +0300

    minor update

--- usersApp/routes/user.routes.js
+++ usersApp/routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router(); 
+const router = express.Router();
 
 const userController = require('../controllers/user.controller');
 const verifyToken = require('../middlewares/auth.middleware').verifyToken;
@@ -7,11 +7,10 @@ const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 
 
 router.get('/',verifyToken, userController.findAll);
-router.get('/:username', userController.findOne);
+router.get('/:username', verifyToken, userController.findOne);
 router.post('/',verifyToken, verifyRoles("ADMIN") ,userController.create);
 router.patch('/:username',verifyToken, verifyRoles("ADMIN"), userController.update); 
 router.delete('/:username',verifyToken, verifyRoles("ADMIN"), userController.deleteByUsername);
-router.delete('/:username/email/:email',verifyToken, verifyRoles("ADMIN"), userController.deleteByEmail);       
+router.delete('/:username/email/:email',verifyToken, verifyRoles("ADMIN"), userController.deleteByEmail);  
 
-
-module.exports = router;
+module.exports = router; 
